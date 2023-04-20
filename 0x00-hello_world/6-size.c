1f7791c36da13c078c1f53bdfb8701b65ceb66f9
Breakthrough <temitopefatoki658@gmail.com>
2023-04-20 13:05:32 -0700

    6-size.c corrected

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
-  * main - printf function with sizeof
-  * Return: always 0 (success)
+  * main - main entry point printf function sizeof
+  * Return: 0 (success)
   */
 int main(void)
 {
-	char: a;
-	int: b;
-	long int: c;
-	long long int: d;
-	float: f;
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
 
 printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(a);
 printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(b);
@@ -17,4 +17,3 @@ printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(c);
 printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(d);
 printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(f);
 return (0);
-}
