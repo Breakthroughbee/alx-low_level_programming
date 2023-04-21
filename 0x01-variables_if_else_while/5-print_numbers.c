fde265358949f10fe5fc3b838c211caec2afc633
Breakthrough <temitopefatoki658@gmail.com>
2023-04-21 08:40:36 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - for loop base ten in printf
+  * Description - 'single digit numbers of base 10'
+  * Return: always 0 (success)
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
