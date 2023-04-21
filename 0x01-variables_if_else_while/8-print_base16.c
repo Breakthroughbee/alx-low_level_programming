5e69b8c697b4441d42d2bd05f15a904dea3a02aa
Breakthrough <temitopefatoki658@gmail.com>
2023-04-21 09:37:17 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  * main - for loop entry point
+  * Description - 'numbers of base 16 in lowercase'
+  * Return: always 0 (success)
+  */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m <= 102; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
