d52b4d6e0ee8ad996307ec89625ba5a02a0c40e5
Breakthrough <temitopefatoki658@gmail.com>
2023-04-21 09:52:42 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  * main - for loop entry point
+  * Description - 'possible combinations of single-digit numbers'
+  * Return: always 0 (success)
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		if (n == 57)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
