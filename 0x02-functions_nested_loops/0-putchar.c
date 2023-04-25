0828e0fd0ad9ee8e58d83a9724442bbed548f37f
Breakthrough <temitopefatoki658@gmail.com>
2023-04-25 04:59:12 -0700

    prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main - prints putchar
+  * Description - putchar with a return value 0
+  * Return: alwys 0 (success)
+  */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
