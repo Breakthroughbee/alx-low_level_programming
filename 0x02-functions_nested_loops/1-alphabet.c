148706374607a84c374c6d2f90b698ca401426e9
Breakthrough <temitopefatoki658@gmail.com>
2023-04-25 09:21:15 -0700

    Lower case alphabet correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
-  * main - Alphabet in lower case entry point
-  * Description -  'Prints alphabet in lowercase, followed by a new line'
-  * Return: 0 (success)
+  * main - alphabet in lowercase
+  * Description - 'prints with putchar'
+  * Return: always 0 (success)
   */
 void print_alphabet(void)
 {
+
 	int i;
 
-	for (i = 97; i = 122; i++)
+	for (i = 97; i <= 122; i++)
 	{
 		_putchar(i);
 	}
