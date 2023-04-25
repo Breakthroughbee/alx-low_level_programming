cfe2a770efcaa9aa55e710d2347b89bf40dac0f6
Breakthrough <temitopefatoki658@gmail.com>
2023-04-25 10:21:09 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - check if is lowercase
+ * @c: is to be checked
+ * Return: 1 if in lowercase, otherwise 0.
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
