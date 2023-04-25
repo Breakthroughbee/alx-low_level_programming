10c2d07add787edad58445770023a536e7dd10e9
Breakthrough <temitopefatoki658@gmail.com>
2023-04-25 09:04:13 -0700

    alphabet in lower case created void

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * main - Alphabet in lower case entry point
+  * Description -  'Prints alphabet in lowercase, followed by a new line'
+  * Return: 0 (success)
+  */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i = 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
