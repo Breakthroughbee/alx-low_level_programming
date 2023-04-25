2261d1d19328c25f29fab069991b289ace799513
Breakthrough <temitopefatoki658@gmail.com>
2023-04-25 09:50:12 -0700

    2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * print_alphabet_x10 - prints alphabet in lower case
+  *
+  * Return: always 0 (success)
+  */
+int print_alphabet_x10(void)
+{
+	int a;
+	int b;
+
+	for (b = 1; a <= 10; a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
