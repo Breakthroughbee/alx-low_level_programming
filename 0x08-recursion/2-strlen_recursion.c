a26a928d69ddeed80e6b65146d41e478f4c166e9
Breakthrough <temitopefatoki658@gmail.com>
2023-05-26 12:33:55 -0700

    String length recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _strlen_recursion - string length entry point
+  *
+  * @s: string
+  *
+  * Return: 0 success
+  */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
