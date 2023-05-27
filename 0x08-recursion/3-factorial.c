416db4199173e3f601ca6707a65f04e4790576e1
Breakthrough <temitopefatoki658@gmail.com>
2023-05-27 03:43:09 -0700

    Recursive factorial of n integer n

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * factorial - factorial of a number
+  *
+  * @n: number
+  *
+  * Return: 0
+  */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	else if (n == 0)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
