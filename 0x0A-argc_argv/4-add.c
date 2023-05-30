e5214b74ad6607706fc4e034d051dc3773403514
Breakthrough <temitopefatoki658@gmail.com>
2023-05-30 06:02:56 -0700

    Addition with argc and argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{
+	int b = 0, a = 0;
+	int add = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (b = 1; b < argc; b++)
+	{
+		while (argv[b][a] != '\0')
+		{
+			if (!isdigit(argv[b][a]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			a++;
+		}
+
+		add = add + atoi(argv[b]);
+	}
+
+	printf("%d\n", add);
+	return (0);
+}
