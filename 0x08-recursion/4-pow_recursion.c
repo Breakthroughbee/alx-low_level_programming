bbdbe97504eb84002cbecbed3be98c11a623d097
Breakthrough <temitopefatoki658@gmail.com>
2023-08-31 10:33:24 -0700

    Power of recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,15 +1,16 @@
 #include "main.h"
 #include <math.h>
+#include <stdio.h>
 
 /**
-  * _pow_recursion - recursion entry point
-  *
-  * @x: integer x
-  *
-  * @y: integer y
-  *
-  * Return: x raised to the power of y
-  */
+ * _pow_recursion - entry
+ *
+ * @x: int x
+ *
+ * @y: int y
+ *
+ * Return: x raised to the power of y (0) success
+ */
 
 int _pow_recursion(int x, int y)
 {
@@ -17,13 +18,10 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-
 	else if (y == 0)
 	{
 		return (1);
 	}
-
 	else
 		return (x * _pow_recursion(x, y - 1));
 }
-
