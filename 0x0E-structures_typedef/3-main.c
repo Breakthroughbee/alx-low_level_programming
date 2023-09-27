9dd3c3055396e3eb5d3ee4cc6be3accd87fe06e5
Breakthrough <temitopefatoki658@gmail.com>
2023-09-27 14:47:33 -0700

    3-main.c

--- 0x0E-structures_typedef/3-main.c
+++ 0x0E-structures_typedef/3-main.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	dog_t my_dog;
+
+	my_dog.name = "Poppy";
+	my_dog.owner = "Bob";
+	my_dog.age = 3.5;
+	printf("My name is %s, %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+	return (0);
+}
