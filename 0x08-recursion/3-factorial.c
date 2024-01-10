c735cbd1ef97c2473556bb9bbcb3c26bff7e9d4e
Breakthrough <temitopefatoki658@gmail.com>
2024-01-10 15:22:29 -0800

    fac

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * factorial - number
+ *
+ * @n: n
+ *
+ * Return: 0
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
