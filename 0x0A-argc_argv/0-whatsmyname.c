687526d918e128e386319d5d1ae360aa8342b880
Breakthrough <temitopefatoki658@gmail.com>
2024-01-12 17:04:11 -0800

    my new name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - arg entry point
+ *
+ * @argc: int
+ *
+ * @argv: array
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
