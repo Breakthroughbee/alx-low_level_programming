014d86ea06139688b2f112c30438bd377ba961b6
Breakthrough <temitopefatoki658@gmail.com>
2024-01-22 11:34:35 -0800

    Free dog

---
+++ 0x0E-structures_typedef/4-main.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	dog_t *my_dog;
+
+	my_dog = new_dog("Poppy", 3.5, "Bob");
+	printf("My name is %s, and i am %.1f\n", my_dog->name, my_dog->age);
+	return (0);
+}
+
