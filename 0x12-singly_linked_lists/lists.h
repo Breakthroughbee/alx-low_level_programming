24ca8052ab840bb18989206aca67e304ade554c1
Breakthrough <temitopefatoki658@gmail.com>
2024-02-10 09:40:17 -0800

    added prototype to header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stddef.h>
 
 int _putchar(char c);
 
@@ -18,4 +19,5 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+size_t print_list(const list_t *h);
 #endif
